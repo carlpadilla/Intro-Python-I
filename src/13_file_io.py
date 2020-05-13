3b23ee895a8a8d4b64198e372089438a3ef27c53
Carl Padilla <carlpadilla@protonmail.com>
2020-05-12 21:10:48 -0400

    progress saved

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -11,9 +11,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+f = open('foo.txt', 'r')
+print(f.readlines())
+f.close()
+
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+e = open('bar.txt', 'w')
+e.write("I\'m learning python\nIt\'s different\nI like Data Science")
+e.close()
+
+e = open('bar.txt', 'r')
+print(e.readlines())
